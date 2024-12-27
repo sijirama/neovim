328d293070eec4a310775340d3c76271940f3f1c
sijirama <gbemilesanmi@gmail.com>
2024-12-27 02:38:13 +0100

    toggle term

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -34,6 +34,20 @@ map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
 map('n', '<leader>fe', ':NvimTreeFocus<CR>', opts)
 map('n', '<leader>ef', ':NvimTreeCollapse<CR>', opts)
 
+--toggle terminal
+vim.keymap.set('n', '<leader>t', ':ToggleTerm direction=float dir=. <CR>', opts)
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
 --NOTE: format code
 vim.api.nvim_set_keymap('n', '<Leader>f', '<cmd>lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
 
