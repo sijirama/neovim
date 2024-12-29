804a96a1fa780ea590d0a0acaa6b58105b5f830f
sijirama <gbemilesanmi@gmail.com>
2024-12-29 22:47:17 +0100

    leap and ufo

--- lua/custom/plugins/ufo.lua
+++ lua/custom/plugins/ufo.lua
@@ -4,6 +4,9 @@ vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
 return {
      'kevinhwang91/nvim-ufo',
      dependencies = {
