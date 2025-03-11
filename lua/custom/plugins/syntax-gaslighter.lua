90842e2a11f035f2ae49bc9d50b525a0f7d003ad
sijirama <gbemilesanmi@gmail.com>
2025-03-11 02:03:40 +0100

    checkpoint

---
+++ lua/custom/plugins/syntax-gaslighter.lua
@@ -0,0 +1,5 @@
+return {
+     dir = '/home/sijirama/Desktop/me/coder/syntax-gaslighting.nvim',
+     cmd = { 'SyntaxGaslighterOn', 'SyntaxGaslighterOff', 'SyntaxGaslighterSayHello' },
+     lazy = false,
+}
