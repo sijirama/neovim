d9bccf73e6a1f1b8e256ad8514b960f37d3a6307
sijirama <gbemilesanmi@gmail.com>
2025-03-18 11:58:08 +0100

    fix: removed middleware to view profile

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,47 +6,47 @@
 --
 require('lazy').setup({
 
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  require 'kickstart/plugins/gitsigns',
-  require 'kickstart/plugins/which-key',
-  require 'kickstart/plugins/telescope',
-  require 'kickstart/plugins/lspconfig',
-  require 'kickstart/plugins/conform',
-  require 'kickstart/plugins/cmp',
-  require 'kickstart/plugins/gruvbox',
-  require 'kickstart/plugins/todo-comments',
-  require 'kickstart/plugins/mini',
-  require 'kickstart/plugins/treesitter',
-  require 'kickstart.plugins.autopairs',
+     'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+     --require 'kickstart/plugins/gitsigns',
+     require 'kickstart/plugins/which-key',
+     require 'kickstart/plugins/telescope',
+     require 'kickstart/plugins/lspconfig',
+     require 'kickstart/plugins/conform',
+     require 'kickstart/plugins/cmp',
+     require 'kickstart/plugins/gruvbox',
+     require 'kickstart/plugins/todo-comments',
+     require 'kickstart/plugins/mini',
+     require 'kickstart/plugins/treesitter',
+     require 'kickstart.plugins.autopairs',
 
-  -- require 'custom/plugins/alpha',
-  -- require 'custom/plugins/fugitive',
-  -- require 'custom/plugins/autotag',
+     -- require 'custom/plugins/alpha',
+     -- require 'custom/plugins/fugitive',
+     -- require 'custom/plugins/autotag',
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  { import = 'custom.plugins' },
+     -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
+     --    This is the easiest way to modularize your config.
+     --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+     { import = 'custom.plugins' },
 }, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
+     ui = {
+          -- If you are using a Nerd Font: set icons to an empty table which will use the
+          -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+          icons = vim.g.have_nerd_font and {} or {
+               cmd = '⌘',
+               config = '🛠',
+               event = '📅',
+               ft = '📂',
+               init = '⚙',
+               keys = '🗝',
+               plugin = '🔌',
+               runtime = '💻',
+               require = '🌙',
+               source = '📄',
+               start = '🚀',
+               task = '📌',
+               lazy = '💤 ',
+          },
+     },
 })
 
 -- vim: ts=2 sts=2 sw=2 et
