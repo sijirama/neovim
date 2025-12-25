bdf332e23b7ea7d5802bfd3a2793c44c834fb408
sijirama <gbemilesanmi@gmail.com>
2025-12-25 18:06:14 +0100

    more cool fixes

--- lua/custom/plugins/use_syntax-gaslighter.lua
+++
@@ -1,5 +0,0 @@
-return {
-     dir = '/home/sijirama/Desktop/me/coder/syntax-gaslighting.nvim',
-     cmd = { 'SyntaxGaslighterOn', 'SyntaxGaslighterOff', 'SyntaxGaslighterSayHello' },
-     lazy = false,
-}
